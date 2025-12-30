c5c4b5af0048490b4ce8707b52876e832ae05898
varun <sjsjd2231@gmail.com>
2025-12-30 11:01:25 +0530

    MOM project

---
+++ MOM/Controllers/MeetingTypeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MOM.Controllers
+{
+    public class MeetingTypeController : Controller
+    {
+        public IActionResult MeetingTypeList()
+        {
+            return View();
+        }
+
+        public IActionResult MeetingTypeAddEdit()
+        {
+            return View();
+        }
+    }
+}
